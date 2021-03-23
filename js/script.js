49d9bd9281948a869f30ac6e41cc2c40578ac323
T0bF0r <tobias.nt.forsberg@gmail.com>
2021-03-23 22:50:18 +0100

    working

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ const fetchTodos = async () => {
   const _todos = await res.json();
 
   todos = _todos;
-  // console.log(todos);
+
 
   listTodos(todos);
 }
@@ -26,84 +26,6 @@ const listTodos = (todos) => {
 
     output.innerHTML += newTodo(todo);
   })
-}
-
-
-
-
-
-
-const newTodo = todo => {
-  let outputCard = todo.completed ?
-    //Strike through text if todo is completed
-    `
-    <div id="${todo.id}" class="todo card completed box">
-      <p class="title card-content p-1 m-1">${todo.title}</p>
-      <div class="card-footer p-1">
-        <button class="card-footer-item button is-primary m-1"><span class="icon is-large"><i class="fas fa-check-square"></i></span></button>
-        <button class="card-footer-item button is-danger m-1"><span class="icon is-medium"><i class="fas fa-trash-alt"></i></span></button>
-      </div>
-    </div>
-
-    `
-    :
-    //Disable trash button if todo not completed 
-    `
-    <div id="${todo.id}" class="todo card box">
-      <p class="title card-content p-1 m-1">${todo.title}</p>
-      <div class="card-footer p-1">
-        <button class="card-footer-item button m-1"><span class="icon is-medium"><i class="far fa-square"></i></span></button>
-        <button class="card-footer-item button m-1" disabled><span class="icon is-medium"><i class="fas fa-trash-alt"></i></span></button>
-      </div>
-    </div>
-    `
-  return outputCard
-}
-
-/*
-const form = document.querySelector('#form');
-const input = document.querySelector('#input');
-const output = document.querySelector('#output');
-
-let todos = []
-
-const fetchTodos = async () => {
-  let url = 'https://jsonplaceholder.typicode.com/todos';
-
-  const res = await fetch(url);
-  const _todos = await res.json();
-
-  todos = _todos;
-  // console.log(todos);
-
-  listTodos(todos);
-}
-fetchTodos();
-
-const listTodos = (todos) => {
-  output.innerHTML = '';
-
-  todos.forEach(todo => {
-
-    output.innerHTML += newTodo(todo);
-  })
-}
-
-const newTodo = todo => {
-
-  let template = todo.completed ? `
-  <div id="${todo.id}" class="todo completed">
-    <h3 class="title">${todo.title}</h3>
-    <button class="btn btn-danger">X</button>
-  </div>
-  `
-  : `
-  <div id="${todo.id}" class="todo">
-    <h3 class="title">${todo.title}</h3>
-    <button class="btn btn-danger">X</button>
-  </div>
-  `
-  return template
 }
 
 const createTodo = async title => {
@@ -124,31 +46,43 @@ const createTodo = async title => {
 
   const todo = await res.json()
 
-  console.log(todo);
   todo.id = Date.now();
 
   todos.unshift(todo)
   listTodos(todos);
 }
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
+const newTodo = todo => {
+  let outputCard = todo.completed ?
+    //Strike through text if todo is completed
+    `
+    <div id="${todo.id}" class="todo card completed box">
+      <p class="title card-content p-1 m-1">${todo.title}</p>
+      <div class="card-footer p-1">
+        
+        <button class="card-footer-item button is-danger m-1"><span class="icon is-medium"><i class="fas fa-trash-alt"></i></span></button>
+      </div>
+    </div>
 
+    `
+    :
+    //Disable trash button if todo not completed 
+    `
+    <div id="${todo.id}" class="todo card box">
+      <p class="title card-content p-1 m-1">${todo.title}</p>
+      <div class="card-footer p-1">
+        
+        <button class="card-footer-item button m-1" disabled><span class="icon is-medium"><i class="fas fa-trash-alt"></i></span></button>
+      </div>
+    </div>
+    `
+  return outputCard
+}
+
+form.addEventListener('submit', e =>{
+  e.preventDefault();
+  console.log('click');
   createTodo(input.value);
   input.value = '';
-})
-
-output.addEventListener('click', e => {
-
-  // console.log(e.target.classList.contains('title'))
-
-  if(e.target.classList.contains('btn-danger'))
-    deleteTodo(e.target.parentNode.id)
-
-})
-
-const deleteTodo = id => {
-  todos = todos.filter(todo => todo.id != id);
-  listTodos(todos);
-}
-*/
+  
+});
